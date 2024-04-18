46f04b96b318c6318acbba1fd3926fe2ea8cea3f
Gobousei <93245017+Gobousei@users.noreply.github.com>
2024-04-18 22:30:10 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -204,10 +204,3 @@
     window.location.reload();
   }
 
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js').then(registration => {
-      console.log('ServiceWorker registration successful.');
-    }).catch(err => {
-      console.log('ServiceWorker registration failed.');
-    });
-  }
